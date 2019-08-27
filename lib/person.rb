262b03bb4b0f7b10ccea4eceb292bc5ccbcadc5c
Wilfred Alec Vernon <wilf.vernon@gmail.com>
2019-08-27 14:16:04 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person
+  def name=(person_name)
+    @name = dog_name
+  end
+  def name
+    @name
+  end
+  def breed=(dog_breed)
+    @breed = dog_breed
+  end
+  def breed
+    @breed
+  end
+end
